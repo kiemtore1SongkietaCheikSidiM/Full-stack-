8a1cea074cce4cb2b73ccd3051ccfd650d5c74be
unknown <skiemto>
2026-02-09 11:25:25 +0000

    new add

--- BACKEND/accounts/models.py
+++ BACKEND/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Parent(models.Model):
+    Name = models.CharField(max_length=100)
+    Prenom = models.CharField(max_length=100)
+    Email = models.CharField()
+    Tel = models.IntegerField()
+    Username = models.CharField(max_length=100)
+    Password = models.CharField(max_length=100)
+
+    
